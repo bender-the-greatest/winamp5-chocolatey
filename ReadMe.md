3bdc92c692e4698759fb77cb1f40bce855a7d1d1
Alexander Alber <alexander.alber@csgi.com>
2016-12-09 08:50:12 -0500

    Added nuspec, install script, and readme

---
+++ ReadMe.md
@@ -0,0 +1,18 @@
+﻿winamp5-chocolatey
+===================
+
+Chocolatey package source for [WinAMP](https://getwinamp.com/).
+
+# Installing from Chocolatey
+`choco install winamp5`
+
+# Building and Instaling the Packages from Source
+
+First, remember to clone the source.
+`git clone https://github.com/metalseargolid/winamp5-chocolatey`
+
+To build and install the package:
+```powershell
+choco pack
+cinst -y ./winamp5.5.666.nupkg
+```
---
+++ tools/chocolateyinstall.ps1
@@ -0,0 +1,12 @@
+﻿$name = 'winamp'
+$installerType = 'exe'
+$silentArgs = '/S'
+$packageSource = 'https://getwinamp.com/files/winamp5666_full_all_redux.exe'
+
+Install-ChocolateyPackage `
+  -PackageName $name `
+  -FileType $installerType `
+  -SilentArgs $silentArgs `
+  -Url $packageSource `
+  -Checksum '820984d46f7a398296994a9173479e47d2aa5b447a474514abbd87f690f72955052e8e38c7ada80ea067c136d584111657191e50fe9202176f98ca88418c22af' `
+  -ChecksumType 'sha512'
